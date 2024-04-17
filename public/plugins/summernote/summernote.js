3c1216bf540156042857f44292b945da4d73d812
gmpero <rostislavpen@gmail.com>
2024-04-17 12:13:57 +0300

    15. Добавляем атрибут content для поста, визуальный редактор Summernote

--- public/plugins/summernote/summernote.js
+++ public/plugins/summernote/summernote.js
@@ -1,14 +1,14 @@
 /*!
- *
+ * 
  * Super simple wysiwyg editor v0.8.18
  * https://summernote.org
- *
- *
+ * 
+ * 
  * Copyright 2013- Alan Hong. and other contributors
  * summernote may be freely distributed under the MIT license.
- *
+ * 
  * Date: 2020-05-20T16:47Z
- *
+ * 
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -2082,7 +2082,7 @@ function dom_html($node, isNewlineOnBlock) {
 function posFromPlaceholder(placeholder) {
   var $placeholder = external_root_jQuery_commonjs2_jquery_commonjs_jquery_amd_jquery_default()(placeholder);
   var pos = $placeholder.offset();
-  var height = $placeholder.outerHeight(true); // includes margin
+  var height = $placeholder.outerHeight(true); // include margin
 
   return {
     left: pos.left,

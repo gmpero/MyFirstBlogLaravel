d0c6d9cc787329694a41c9af3d1822358e1f1a28
gmpero <rostislavpen@gmail.com>
2024-04-18 18:35:44 +0300

    21. Создаем класс Service для рефакторинга создания и обновления постов

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -10,24 +10,12 @@ use Illuminate\Support\Facades\Storage;
 use function Laravel\Prompts\alert;
 
 
-class StoreController extends Controller
+class StoreController extends BaseController
 {
     public function __invoke(StoreRequest $request)
     {
-        try {
-            $data = $request->validated();
-
-            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
-            $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
-
-            $tagIds = $data['tag_ids'];
-            unset($data['tag_ids']);
-
-            $post = Post::firstOrCreate($data);
-            $post->tags()->attach($tagIds);
-        } catch (\Exception $exception) {
-            alert(404);
-        }
+        $data = $request->validated();
+        $this->service->store($data);
 
         return redirect()->route('admin.post.index');
     }

c8a03eb23c609279a2e5325c60fb621a3e320d42
gmpero <rostislavpen@gmail.com>
2024-04-18 14:07:19 +0300

    19. Реализуем апдейт у постов, форма редактирования, отображение изображений

--- resources/views/Admin/post/edit.blade.php
+++ resources/views/Admin/post/edit.blade.php
@@ -1,49 +1,112 @@
 @extends('admin.layouts.main')
 @section('content')
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">Редактирование поста</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard v1</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Редактирование поста</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v1</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
 
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <!-- Small boxes (Stat box) -->
-            <div class="row">
-                <div class="col-12">
-                    <form action="{{route('admin.post.update', $post->id)}}" method="POST" class="w-25">
-                        @csrf
-                        @method('PATCH')
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="title" placeholder="Название категории" value="{{$post->title}}">
-                            @error('title')
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+                    <div class="col-12">
+                        <form action="{{route('admin.post.update', $post->id)}}" method="POST" enctype="multipart/form-data" class="w-75">
+                            @csrf
+                            @method('PATCH')
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="title" placeholder="Название поста"
+                                       value="{{$post->title}}">
+                                @error('title')
                                 <div class="text-danger">Это поле необходимо заполнить!</div>
-                            @enderror
-                        </div>
-                        <input type="submit" class="btn btn-primary" value="Изменить">
-                    </form>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <textarea id="summernote" name="content">
+                                    {{$post->content}}
+                                </textarea>
+                                @error('content')
+                                <div class="text-danger">Это поле необходимо заполнить!</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="preview_image">Добавить превью</label>
+                                <div class="w-25 mb-2">
+                                    <img src="{{asset('storage/'.$post->preview_image)}}" alt="preview_image" class="w-75">
+                                </div>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="preview_image">
+                                        <label class="custom-file-label">Выберете изображение</label>
+                                    </div>
+                                </div>
+                                @error('preview_image')
+                                <div class="text-danger">Это поле необходимо заполнить!</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="main_image">Добавить главное изображение</label>
+                                <div class="w-25 mb-2">
+                                    <img src="{{asset('storage/'.$post->main_image)}}" alt="main_image" class="w-75">
+                                </div>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="main_image">
+                                        <label class="custom-file-label">Выберете изображение</label>
+                                    </div>
+                                </div>
+                                @error('main_image')
+                                <div class="text-danger">Это поле необходимо заполнить!</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>Выберите категорию</label>
+                                <select name="category_id" class="form-control">
+                                    @foreach($categories as $category)
+                                        <option value="{{$category->id}}"
+                                            {{$category->id == $post->category_id?' selected':''}}
+                                        >{{$category->title}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Multiple</label>
+                                <select class="select2" name="tag_ids[]" multiple="multiple"
+                                        data-placeholder="Выберите теги"
+                                        style="width: 100%;">
+                                    @foreach($tags as $tag)
+                                        <option value="{{$tag->id}}"
+                                            {{is_array($post->tags->pluck('id')->toArray())&&in_array($tag->id,$post->tags->pluck('id')->toArray())?' selected':''}}
+                                        >{{$tag->title}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" value="Изменить">
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-            <!-- /.row -->
+                <!-- /.row -->
 
-        </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-</div>
-<!-- /.content-wrapper -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
 @endsection

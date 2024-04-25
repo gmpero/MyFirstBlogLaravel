1b23da1f1b23ddd6ddeaf2a4f3098da6a0bfa351
gmpero <rostislavpen@gmail.com>
2024-04-25 11:37:06 +0300

    32. Создаем личный кабинет пользователя

---
+++ resources/views/personal/includes/sidebar.blade.php
@@ -0,0 +1,30 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <div class="sidebar">
+        <ul class="pt-5 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item">
+                <a href="{{route('personal.main.index')}}" class="nav-link">
+                    <i class="nav-icon fas fa-home"></i>
+                    <p>
+                        Главная
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{route('personal.liked.index')}}" class="nav-link">
+                    <i class="nav-icon fas fa-heart"></i>
+                    <p>
+                        Понравившиеся посты
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{route('personal.comment.index')}}" class="nav-link">
+                    <i class="nav-icon fas fa-comment"></i>
+                    <p>
+                        Мои комментарии
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</aside>
